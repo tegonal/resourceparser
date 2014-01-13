847acfd35003e94b1f0e3eb26e4590f34361d264
Thomas Rawyler <thomas.rawyler@tegonal.com>
2014-01-13 08:47:54 +0100

    scala style

--- src/main/scala/com/tegonal/resourceparser/generator/ResourceBundleTree.scala
+++ src/main/scala/com/tegonal/resourceparser/generator/ResourceBundleTree.scala
@@ -13,10 +13,10 @@ class ResourceBundleTree {
         resources.put(Path(Nil), ResourceNodeBuffer(Nil, ListBuffer.empty))
 
         // ignoring comments so far
-        elements.collect { case e: Property => e }.map(createProperty(_))
+        elements collect { case e: Property => e } map (createProperty(_))
       }
     }
-    
+
     resources(Path(Nil)).toResourceNode
   }
 
@@ -24,17 +24,17 @@ class ResourceBundleTree {
   def createProperty(property: Property) = property match {
     case Property(path, _) =>
       // ResourceNode already exists
-      resources.get(path).map { existing =>
+      resources.get(path) map { existing =>
         // just update the path
         existing.isProperty = true
-      }.getOrElse {
+      } getOrElse {
         // create the ResourceNode
-        val created = ResourceNodeBuffer(path.pathElements.map(_.name), ListBuffer.empty, true)
+        val created = ResourceNodeBuffer(path.pathElements map (_.name), ListBuffer.empty, true)
         resources.put(path, created)
         // add child to parent
-        resources.get(Path(path.pathElements.init)).map { parent =>
+        resources.get(Path(path.pathElements.init)) map { parent =>
           parent.children += created
-        }.getOrElse {
+        } getOrElse {
           createHierarchy(Path(path.pathElements.init), created)
         }
       }
@@ -42,10 +42,10 @@ class ResourceBundleTree {
 
   def createHierarchy(path: Path, child: ResourceNodeBuffer): Unit = path match {
     case Path(pathElements) => {
-      resources.get(path).map { existing =>
+      resources.get(path) map { existing =>
         existing.children += child
-      }.getOrElse {
-        val created = ResourceNodeBuffer(pathElements.map(_.name), ListBuffer.empty += child)
+      } getOrElse {
+        val created = ResourceNodeBuffer(pathElements map (_.name), ListBuffer.empty += child)
         resources.put(path, created)
         createHierarchy(Path(pathElements.init), created)
       }
